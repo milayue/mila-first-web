f0595dc731afab52a7b357e0273908e14a8aca8f
Mila <mila.zhang2019@users.noreply.github.com>
2020-09-03 23:25:45 +0800

    first commit

---
+++ tutorial.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var num = 5;
+
+const a = 5;
+const obj = {};
+
+obj.x = 'string';
+obj.y = [34,'cfc'];
+
+var v = {
+    name: 'Sunce',
+    value: true
+}
+
+var people = [{
+    first_name = 'K',
+    last_name = 'H'
+},{
+    first_name = 'L',
+    last_name = 'P'
+}]
+
+console.log(people[0].name)
+
+// ==
+// ===
+
